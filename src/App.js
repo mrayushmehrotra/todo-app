2cc28f0c3ce43085037d3c750f44ab6802ce05cf
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2024-07-13 17:53:14 +0530

    Created a Simple To Do List Application with React & Redux.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import { Container, Typography } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center" gutterBottom>
+        To-Do List
+      </Typography>
+      <TaskInput />
+      <TaskList />
+    </Container>
   );
 }
 
